5c5340659e8aab5419ee7b3c0a7d649bc8ead801
Mr O <ryan.p.osterberg@gmail.com>
2018-11-09 08:38:13 -0600

    First Section of Topics

---
+++ src/input/HelloWorldInput.java
@@ -0,0 +1,11 @@
+package input;
+
+public class HelloWorldInput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Helloo World!!");
+
+	}
+
+}
---
+++ src/oOP/HelloWorldOOP.java
@@ -0,0 +1,11 @@
+package oOP;
+
+public class HelloWorldOOP {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Helloo World!!");
+
+	}
+
+}

4fe9c9f84e6133a421367ee744f4bdac25ee299c
Mr O <ryan.p.osterberg@gmail.com>
2018-11-09 08:43:39 -0600

    Second Section of Topics

---
+++ src/randomNumbers/HelloWorldRandom.java
@@ -0,0 +1,11 @@
+package randomNumbers;
+
+public class HelloWorldRandom {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Helloo World!!");
+
+	}
+
+}

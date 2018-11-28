17274ac59f218b018adf2cda66dbf9d1e8c751ce
Matthew Mauermann <mauermann5@outlook.com>
2018-11-28 08:16:17 -0600

    Saved changes

--- src/mathOperators/speedingTicket.java
+++ src/mathOperators/speedingTicket.java
@@ -1,10 +1,26 @@
 package mathOperators;
 
+import java.util.Scanner;
+
 public class speedingTicket {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		double speed = -999;
+		double speedlimit = -999;
+		double ticket = -999;
+		System.out.println("What is the speed limit?");
+		speedlimit = input.nextDouble();
+		System.out.println("What is your speed?");
+		speed = input.nextDouble();
+		ticket = speed - speedlimit;
+		ticket = ticket - 1;
+		ticket = ticket * 40;
+		ticket = ticket + 55;
+		System.out.println("Your speed is " + speed);
+		System.out.println("The speed limit is " + speedlimit);
+		System.out.println("The price of your ticket is " + ticket);
 	}
 
 }

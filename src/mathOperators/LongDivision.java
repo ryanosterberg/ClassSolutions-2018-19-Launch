79e1ba07a0c37f9ea024c7a3f16fc1a6b1260e9a
b1zt_mcha31 <mickedylinnihan@gmail.com>
2018-11-30 08:15:15 -0600

    LongDIv

--- src/mathOperators/LongDivision.java
+++ src/mathOperators/LongDivision.java
@@ -1,5 +1,15 @@
 package mathOperators;
 
-public class LongDivision {
+import java.util.Scanner;
 
+public class LongDivision {
+	public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	int dividend = input.nextInt();
+	int divisor= input.nextInt();
+	int answer = dividend/divisor;
+	int remander = dividend % divisor; 
+	System.out.println(answer+ "r"+ remander );
+
+}
 }
